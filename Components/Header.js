91af2dd87aea24d918617bb24653cdd8fbf4cb9b
SAFRASSALIHEEN <safrassaliheen456@gmail.com>
2023-10-12 00:11:23 +0530

    changes 1

---
+++ Components/Header.js
@@ -0,0 +1,44 @@
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { FontAwesome5 } from "@expo/vector-icons";
+
+const Header = () => {
+  return (
+    <View
+      style={{
+        backgroundColor: "#003580",
+        height: 65,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-around",
+      }}
+    >
+      <Pressable
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          borderColor: "white",
+          borderWidth: 2,
+          borderRadius: 25,
+          padding: 8,
+        }}
+      >
+        <FontAwesome5 name="hotel" size={26} color="white" />
+        <Text
+          style={{
+            marginLeft: 8,
+            fontWeight: "bold",
+            color: "white",
+            fontSize: 15,
+          }}
+        >
+          Hotels
+        </Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default Header;
+
+const styles = StyleSheet.create({});
---
+++ Screen/HomeScreen.js
@@ -0,0 +1,45 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import Header from "../Components/Header";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      title: "Online Hotel Booking",
+      headerTitleStyle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "white",
+      },
+      headerStyle: {
+        backgroundColor: "#003580",
+        height: 110,
+        borderBottomColor: "transparent",
+        shadowColor: "transparent",
+      },
+      headerRight: () => (
+        <Ionicons
+          name="notifications-outline"
+          size={24}
+          color="white"
+          style={{ marginRight: 12 }}
+        />
+      ),
+    });
+  }, []);
+
+  return (
+    <View>
+      <Header />
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
