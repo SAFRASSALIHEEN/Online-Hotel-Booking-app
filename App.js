6d2c761e373c95aa41825f97d8fb9a4d0bbc1906
SAFRASSALIHEEN <safrassaliheen456@gmail.com>
2023-11-11 14:59:14 +0530

    new commit

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import StackNavigator2 from "../my-app/StackNavigator2";
+import { ModalPortal } from "react-native-modals";
 
 export default function App() {
   return (
     <>
       <StackNavigator2 />
+      <ModalPortal />
     </>
   );
 }
